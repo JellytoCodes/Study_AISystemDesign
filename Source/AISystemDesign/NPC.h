3d85103c761efd172eb56c9c371b4b59fa2811af
Jellytooooooo <taegu234@naver.com>
2025-06-03 10:42:13 +0900

    250603 스터디 시작

---
+++ Source/AISystemDesign/NPC.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "NPC.generated.h"
+
+UCLASS()
+class AISYSTEMDESIGN_API ANPC : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ANPC();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+};
